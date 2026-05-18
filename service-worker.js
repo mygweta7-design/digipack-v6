036d8ea3ac7f6e08ce5f7defb646b7bb01dd0ea1
mygweta7-design <mygweta7@gmail.com>
2026-05-18 15:18:15 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-// DIGIPACK V11 - no cache service worker
+// DIGIPACK V12 - stable no cache service worker
 self.addEventListener('install', e=>self.skipWaiting());
 self.addEventListener('activate', e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', e=>e.respondWith(fetch(e.request,{cache:'no-store'})));
